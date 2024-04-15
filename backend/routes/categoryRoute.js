6ac3d9f8d888bad8b265b84547d4a0f470c32c6c
ram-sah <mohan2036@gmail.com>
2024-04-15 17:26:50 -0400

    added update/get category in backend

--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,10 +1,19 @@
 import express from "express"
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { CreateCategoryController } from "../controllers/categoryController.js";
+import { categoryController, CreateCategoryController, singleCategoryController, updateCategoryController } from "../controllers/categoryController.js";
 
 const router = express.Router()
 
 //routes
-router.post('create-category', requireSignIn, isAdmin, CreateCategoryController)
+//create category
+router.post('/create-category', requireSignIn, isAdmin, CreateCategoryController);
+
+//update category
+ router.put('/update-category/:id', requireSignIn, isAdmin, updateCategoryController);
+
+//get all category
+router.get('/get-category', categoryController)
+//Single category
+router.get('/single-category/:slug', singleCategoryController)
 
 export default router;
