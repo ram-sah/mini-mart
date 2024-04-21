9f466bc79e371c56674911ea7520ef186d3d2120
ram-sah <mohan2036@gmail.com>
2024-04-20 23:59:19 -0400

    added update & delete functionality on frontend

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,60 +1,110 @@
-import React, { useState } from 'react'
-import AdminMenu from '../../components/Layout/AdminMenu'
-import Layout from '../../components/Layout/Layout';
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import { useEffect } from 'react';
-import CategoryForm from '../../components/Form/CategoryForm';
+import React, { useState } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useEffect } from "react";
+import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CreateCategory = () => {
     const [categories, setCategories] = useState([]);
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [updatedName, setUpdatedName] = useState("");
+
     //handle form
     const handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         try {
-            const { data } = await axios.post('/api/v1/category/create-category', { name })
+            const { data } = await axios.post("/api/v1/category/create-category", {
+                name,
+            });
             if (data?.success) {
-                toast.success(`${name} is created`)
+                toast.success(`${name} is created`);
                 getAllCategory();
-
             } else {
-                toast.error(data.message)
+                toast.error(data.message);
             }
         } catch (error) {
-            console.log(error)
-            toast.error("Something went wrong in input form")
+            console.log(error);
+            toast.error("Something went wrong in input form");
         }
-    }
+    };
 
     //get all category
     const getAllCategory = async () => {
         try {
-            const { data } = await axios.get("/api/v1/category/get-category")
+            const { data } = await axios.get("/api/v1/category/get-category");
             if (data.success) {
-                setCategories(data.category)
+                setCategories(data.category);
             }
         } catch (error) {
-            console.log(error)
-            toast.error("Something went wrong in getting category")
+            console.log(error);
+            toast.error("Something went wrong in getting category");
+        }
+    };
+    useEffect(() => {
+        getAllCategory();
+    }, []);
+
+    //update category
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.put(
+                `/api/v1/category/update-category/${selected._id}`,
+                { name: updatedName }
+            );
+            if (data.success) {
+                toast.success(`${updatedName} is updated`);
+                setSelected(null);
+                setUpdatedName("");
+                setVisible(false);
+                getAllCategory();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error("something went wrong");
         }
     }
-    useEffect(() => {
-        getAllCategory()
-    }, [])
+    //delete category
+    const handleDelete = async (pId) => {
+        try {
+            const { data } = await axios.delete(
+                `/api/v1/category/delete-category/${pId}`
+            );
+            if (data.success) {
+                toast.success(`category is deleted`);
+
+                getAllCategory();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error("Somtihing went wrong");
+        }
+    };
+
     return (
         <Layout title={"Dashboard - Create Category "}>
-            <div className='container-fluid m-3 p-3'>
-                <div className='row'>
-                    <div className='col-md-3'>
+            <div className="container-fluid p-3">
+                <div className="row">
+                    <div className="col-md-3">
                         <AdminMenu />
                     </div>
-                    <div className='col-md-9'>
+                    <div className="col-md-9">
                         <h1>Manage Category </h1>
                         <div className="p-3 w-50">
-                            <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName} />
+                            <CategoryForm
+                                handleSubmit={handleSubmit}
+                                value={name}
+                                setValue={setName}
+                            />
                         </div>
-                        <div className='w-75'>
+                        <div className="w-75">
                             <table className="table">
                                 <thead>
                                     <tr>
@@ -63,13 +113,24 @@ const CreateCategory = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {categories?.map(e => (
+                                    {categories?.map((e) => (
                                         <>
                                             <tr>
                                                 <td key={e._id}> {e.name} </td>
                                                 <td>
-                                                    <button className='btn btn-primary ms-2'>Edit</button>
-                                                    <button className='btn btn-danger ms-2'>Danger</button>
+                                                    <button
+                                                        className="btn btn-primary ms-2"
+                                                        onClick={() => {
+                                                            setVisible(true);
+                                                            setUpdatedName(e.name);
+                                                            setSelected(e);
+                                                        }}
+                                                    >
+                                                        Edit
+                                                    </button>
+                                                    <button className="btn btn-danger ms-2 " onClick={() => { handleDelete(e._id) }}>
+                                                        Delete
+                                                    </button>
                                                 </td>
                                             </tr>
                                         </>
@@ -77,11 +138,22 @@ const CreateCategory = () => {
                                 </tbody>
                             </table>
                         </div>
+                        <Modal
+                            onCancel={() => setVisible(false)}
+                            footer={null}
+                            visible={visible}
+                        >
+                            <CategoryForm
+                                value={updatedName}
+                                setValue={setUpdatedName}
+                                handleSubmit={handleUpdate}
+                            />
+                        </Modal>
                     </div>
                 </div>
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default CreateCategory
+export default CreateCategory;
--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -5,7 +5,7 @@ import Layout from '../../components/Layout/Layout'
 const CreateProduct = () => {
     return (
         <Layout title={"Dashboard - Create Product "}>
-            <div className='container-fluid m-3 p-3'>
+            <div className='container-fluid p-3'>
                 <div className='row'>
                     <div className='col-md-3'>
                         <AdminMenu />
